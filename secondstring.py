bfeb3c41277c09e8023c6bb26a415a7e5d622e41
Conor O'Riordan <oriordanc@office.local>
2020-01-30 16:47:12 +0000

    Week 3 task

--- secondstring.py
+++ secondstring.py
@@ -1,8 +1,5 @@
 # to reverse the string of reciebved user input"
 
-#sentence = input("Please enter a sentence: ")
+sentence = input("Please enter a sentence: ")
 
-s = "The quick brown fox jumps over the lazy dog."
-
-
-print(s[-1::-2])
+print(sentence[-1::-2])
