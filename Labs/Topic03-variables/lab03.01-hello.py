dbf93c3da097f4055b25caf44084d0897599481c
Conor O'Riordan <oriordanc@office.local>
2020-01-30 22:30:13 +0000

    lab 1 and 2

--- Labs/Topic03-variables/lab03.01-hello.py
+++ Labs/Topic03-variables/lab03.01-hello.py
@@ -1,6 +1,6 @@
 #Reads in name and outputs hello
 
 name = input("Enter your name: ")
-
+sn = "O'Riordan"
 #print("Hello " + name)
-print("Hello {}".format(name))
+print("Hello {} {}".format(name,sn))
--- Labs/Topic03-variables/lab03.02-sub.py
+++ Labs/Topic03-variables/lab03.02-sub.py
@@ -1,1 +1,8 @@
-#
+#subtract 2 nums
+x = int(input("Enter first number :"))
+y = int(input("Enter second number"))
+
+answer = x - y
+
+print("{} minus {} is {}".format(x,y,answer))
+print(x,"minus",y,"is",answer)
