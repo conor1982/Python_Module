aa52f41de4f8eccf1c23d77d17a732fb2ee27651
Conor O'Riordan <oriordanc@office.local>
2020-02-07 11:00:24 +0000

    Task 4

--- 3_Collatz2.py
+++ 3_Collatz2.py
@@ -4,31 +4,22 @@
 # if it is even, divide it by two, but if it is odd, multiply it by three and add one.
 # Have the program end if the current value is one.
 
+#Version includes print text to describe number input
+#Version includes print text to describe number output
+
 #Enter positive number
-pos_int = int(input("Please enter a positive integer: "))
+pos_int = int(input("Please enter a positive integer:"))
 
 #While Loop
 #If Statement to calculate based on Odd or Even Number
  
 while pos_int > 1:
     if pos_int % 2 == 0:
-        print(round(pos_int), end = " ")
+        print(round(pos_int),"is even so divide by two. This gives:",round(pos_int/2))
         pos_int = pos_int/2
-    
-    elif pos_int % 2 != 0: 
-        print(round(pos_int), end = " ")
+    else:
+        print(round(pos_int),"is odd so multiply by three and add one. This gives:",round((pos_int*3)+1))
         pos_int = (pos_int*3)+1
 
-print(round(pos_int))
-
-    
-
 #Prints when program completes
-#print("We have now reached Number One!!! Programme completed")
-
-    
-    
-
-    
-
-  
+print("We have now reached",round(pos_int),"!!!! Programme completed")
