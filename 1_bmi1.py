a84b351e9cb3adf306949719f08b7eacad3956b9
Conor O'Riordan <oriordanc@office.local>
2020-02-10 11:07:06 +0000

    BMI Task

--- 1_bmi1.py
+++ 1_bmi1.py
@@ -1,12 +1,12 @@
 #bmi calculator
 #Enter Height: 
 
-Height = float(input("Enter Height in CM: "))/100
-Weight = float(input("Enter Weight in KG: "))
+Weight = float(input("Enter Weight (in KG): "))
+Height = float(input("Enter Height (in CM): "))/100
 
 #Below takes input from variables above and does calculation
 bmi = round((Weight/(Height*Height)),2)
 
 #prints answer
-print("Your BMI is",bmi)
+print("BMI is",bmi)
 
