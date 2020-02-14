5518a806e2a012da21a1540fb792bf3a57a5c884
Conor O'Riordan <oriordanc@office.local>
2020-02-14 10:30:33 +0000

    Weekly Task

--- 4_Weekday.py
+++ 4_Weekday.py
@@ -5,13 +5,17 @@
 import datetime as dt
 
 #weekend day numbers
-weekend = [4,5]
+weekend = [5,6]
+Fri = 4
 
 #is today day number in variable weekend
 
 if dt.datetime.today().weekday() in weekend:
 #if day number 4 0r 5 its the weekend    
     print("It is the weekend, yay!")
+
+elif dt.datetime.today().weekday() == 4:
+    print("Its is a weekday but it at least it is Friday!!!!")
 
 #if day number not in weekend variable prints weekday message
 else:
---
+++ prime.py
@@ -0,0 +1,23 @@
+#list variable
+
+P = []
+
+upper = 100
+
+
+for i in range(2,upper):
+
+    isprime = True
+
+    for j in P:
+        if i % j == 0:
+            isprime = False
+
+            break
+    if isprime:
+        P.append(i)
+
+print(P)
+
+print(sum(P))
+
