4a82c1c037f91634e97c0469428f8ac8b637ed51
Conor O'Riordan <oriordanc@office.local>
2020-03-04 15:06:16 +0000

    Task 4

--- 3_Collatz.py
+++ 3_Collatz.py
@@ -1,4 +1,5 @@
 #Conor O'Riordan
+#Collatz Task
 # Write a program that asks the user to input any positive integer and outputs the successive values of the following calculation.
 # At each step calculate the next value by taking the current value and
 # if it is even, divide it by two, but if it is odd, multiply it by three and add one.
