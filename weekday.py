a67c8568d6f9c932dba69dadf990235aa7c75ae2
Conor O'Riordan <oriordanc@office.local>
2020-03-24 15:52:11 +0000

    Task 5 weekday

--- weekday.py
+++ weekday.py
@@ -8,14 +8,17 @@ import datetime as dt
 #weekend day numbers in list
 weekend = [5,6]
 
+#variable that returns number value for current day based on time program ran 0 = Monday.... 6 = Sunday
+today = dt.datetime.today().weekday()
+
 #is today day number in variable weekend
 
-if dt.datetime.today().weekday() in weekend:
+if today in weekend:
 #if day number 5 0r 6 its the weekend    
     print("It is the weekend, yay!")
 
 #if day number is 4 add message about it being Friday
-elif dt.datetime.today().weekday() == 4:
+elif today == 4:
     print("Its is a weekday but it at least it is Friday!!!!")
 
 #if day number not in weekend variable prints weekday message
