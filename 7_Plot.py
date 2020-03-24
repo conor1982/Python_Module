413a7362ebf4a634a55272e2c0eeb89b918b1deb
Conor O'Riordan <oriordanc@office.local>
2020-03-24 09:20:19 +0000

    Task 7

--- 7_Plot.py
+++ 7_Plot.py
@@ -1,19 +1,23 @@
 import matplotlib.pyplot as plt
 import numpy as np
+
 x = np.arange(0,4.0)
 
 fx = x
 gx = x**2
 hx = x**3
 
+
 #https://matplotlib.org/api/_as_gen/matplotlib.pyplot.plot.html
 
-plt.plot(x,fx,'--k',label = "f(x)",linewidth = 2)
-plt.plot(x,gx,'--b', label = "g(x)", linewidth = 3)
-plt.plot(x,hx, '--r', label = 'h(x)', linewidth = 4)
+plt.plot(x,fx,'--k',label = "f(x)",linewidth = 2,marker = '.',markersize = 12 )
+plt.plot(x,gx,'--b', label = "g(x)", linewidth = 2,marker = '.', markersize = 12)
+plt.plot(x,hx, '--r', label = 'h(x)', linewidth = 2, marker = '.',markersize = 12 )
 plt.legend()
-plt.title('Weekly Task 8')
+plt.title('Weekly Task 7')
 plt.xlabel('Range')
 plt.ylabel('Function Value')
-plt.savefig("Task 8")
 
+plt.savefig("Task 7")
+
+plt.show()
