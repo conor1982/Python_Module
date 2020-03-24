005085dcf74053c03e4c7d1a6a4f88adc32c74f6
Conor O'Riordan <oriordanc@office.local>
2020-03-24 10:34:25 +0000

    Task 2 BMI

--- 1_bmi.py
+++ 1_bmi.py
@@ -1,14 +1,35 @@
 #Conor O'Riordan
-#Task 2 BMI calculator
+#Weekly Task 2 BMI calculator
+
+#while loop for error handling
+#Ref: https://docs.python.org/3.1/tutorial/errors.html
+#Ref: Topic 9: Errors lecture videos
+while True:
+    try:
+        #Enter Height - user input 
+        weight = float(input("Enter Weight (in KG): "))
+        break
+
+    #Error printed if non number entered as input   
+    except ValueError:
+        print("The input was not a valid number. Please try again!")
 
 
-#Enter Height - user input
-#Enter Weight - user input
-Weight = float(input("Enter Weight (in KG): "))
-Height = float(input("Enter Height (in CM): "))/100
+#while loop for error handling
+#Ref: https://docs.python.org/3.1/tutorial/errors.html
+#Ref: Topic 9: Errors lecture videos
+while True:
+    try:
+        #Enter Weight - user input
+        height = float(input("Enter Height (in CM): "))/100
+        break
 
+    #Error printed if not a number entered as input
+    except ValueError:
+        print("The input was not a valid number. Please try again!")
+   
 #Below takes input from variables above and does calculation
-bmi = round((Weight/(Height*Height)),2)
+bmi = round((weight/(height*height)),2)
 
 #prints answer
 print("BMI is",bmi)
